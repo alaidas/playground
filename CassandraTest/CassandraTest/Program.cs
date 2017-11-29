72915291fd850747ce0d07da2150b0b6cb2a17cc
Aidas Aleksiejus <aia@danskebank.lt>
2017-11-29 14:51:48 +0200

    Initial2

---
+++ CassandraTest/CassandraTest/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+using Cassandra;
+
+namespace CassandraTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cluster cluster = Cluster.Builder().AddContactPoint("10.132.71.22").Build();
+            ISession session = cluster.Connect("demo");
+
+            string guid = Guid.NewGuid().ToString();
+            session.Execute($"insert into users (id, lastname, age, city, email, firstname) values ('{guid}', 'Jones', 35, 'Austin', 'bob@example.com', 'Bob')");
+
+            Row result = session.Execute($"select * from users where id='{guid}'").First();
+            Console.WriteLine("{0} {1}", result["firstname"], result["age"]);
+
+            session.Execute($"update users set age = 36 where id = '{guid}'");
+            result = session.Execute($"select * from users where id='{guid}'").First();
+            Console.WriteLine("{0} {1}", result["firstname"], result["age"]);
+
+            session.Execute($"delete from users where id = '{guid}'");
+
+            RowSet rows = session.Execute("select * from users");
+            foreach (Row row in rows)
+                Console.WriteLine("{0} {1}", row["firstname"], row["age"]);
+
+            Console.ReadLine();
+        }
+    }
+}
