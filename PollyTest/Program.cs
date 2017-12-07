f3f2382215d487e1b5c65d605509393b77d61aa2
Aidas Aleksiejus <aia@danskebank.lt>
2017-12-07 14:24:48 +0200

    Added PollyTest project.

---
+++ PollyTest/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Polly;
+using System.Threading.Tasks;
+
+namespace PollyTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Running...");
+
+            RunAsync().Wait();
+
+            Console.WriteLine("Stopped");
+        }
+
+        static async Task RunAsync()
+        {
+            try
+            {
+                await Policy.Handle<Exception>()
+                .RetryAsync(2, (ex, retryCount) =>
+                {
+                    Console.WriteLine($"Count = {retryCount}");
+                }).ExecuteAsync(() =>
+                {
+                    Console.WriteLine("Executing");
+                    throw new Exception("This is moin");
+                });
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine($"Exception {ex.Message}");
+            }
+        }
+    }
+}
